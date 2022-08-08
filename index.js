0dd6b7b197f0434a8ba99c32d5c0ba7a7ff0b129
Glitch (im-blue-be) <none>
2022-08-08 04:52:36 +0000

    💸👐 Checkpoint ./package.json:47469336/101:47469413/3455 ./index.js:47469336/740:47469413/7030

--- index.js
+++ index.js
@@ -1,80 +1,112 @@
-const Discord = require('discord.js-selfbot-v13');
-const config = require('./config');
-const handler = require('./src/commandHandler');
-const { DisTube } = require('distube');
-const client = new Discord.Client();
+const Discord = require("discord.js-selfbot-v13");
+const config = require("./config");
+const handler = require("./src/commandHandler");
+const { DisTube } = require("distube");
+const client = new Discord.Client({
+  checkUpdate: false,
+});
+const { SpotifyPlugin } = require("@distube/spotify");
+
+const { SoundCloudPlugin } = require("@distube/soundcloud");
+
+const { YtDlpPlugin } = require("@distube/yt-dlp");
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-client.emotes = require('./config.json').emoji;
+client.emotes = require("./config.json").emoji;
 client.distube = new DisTube(client, {
-	leaveOnStop: false,
-	emitNewSongOnly: true,
-	emitAddSongWhenCreatingQueue: false,
-	emitAddListWhenCreatingQueue: false,
+  leaveOnEmpty: false,
+  leaveOnFinish: false,
+  leaveOnStop: false,
+  emitNewSongOnly: true,
+  emitAddSongWhenCreatingQueue: true,
+  emitAddListWhenCreatingQueue: true,
+  plugins: [
+    new SoundCloudPlugin(),
+    new SpotifyPlugin({
+      emitEventsAfterFetching: false,
+      parallel: false,
+      api: {
+        clientId: process.env.client,
+        clientSecret: process.env.secret,
+      },
+    }),
+    new YtDlpPlugin({
+      update: false,
+    }),
+  ],
+  savePreviousSongs: true,
 });
 client
-	.on('ready', async () => {
-		console.log(`Logged in as ${client.user.tag}!`);
-		// Register all commands
-		await handler(client);
-	})
-	.on('messageCreate', async (message) => {
-		if (
-			!message.guild ||
-			message.author.bot ||
-			!message.content?.startsWith(config.prefix)
-		)
-			return;
-		const args = message.content.trim().slice(config.prefix.length).split(/ +/);
-		const commandName = args.shift().toLowerCase();
-		const command =
-			(await client.commands.get(commandName)) ||
-			(await client.commands.get(await client.aliases.get(commandName)));
-		if (!command) return;
-		try {
-			await command.run(client, message, args);
-		} catch (e) {
-			console.error(e);
-		}
-	})
-	.login(process.env.token);
-    
+  .on("ready", async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    // Register all commands
+    await handler(client);
+  })
+  .on("messageCreate", async (message) => {
+    if (
+      !message.guild ||
+      message.author.bot ||
+      !message.content?.startsWith(config.prefix)
+    )
+      return;
+    const args = message.content.trim().slice(config.prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    const command =
+      (await client.commands.get(commandName)) ||
+      (await client.commands.get(await client.aliases.get(commandName)));
+    if (!command) return;
+    try {
+      await command.run(client, message, args);
+    } catch (e) {
+      console.error(e);
+    }
+  })
+  .login(process.env.token);
+
 const status = (queue) =>
-	`Volume: \`${queue.volume}%\` | Filter: \`${
-		queue.filters.join(', ') || 'Off'
-	}\` | Loop: \`${
-		queue.repeatMode
-			? queue.repeatMode === 2
-				? 'All Queue'
-				: 'This Song'
-			: 'Off'
-	}\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``;
+  `Volume: \`${queue.volume}%\` | Filter: \`${
+    queue.filters.join(", ") || "Off"
+  }\` | Loop: \`${
+    queue.repeatMode
+      ? queue.repeatMode === 2
+        ? "All Queue"
+        : "This Song"
+      : "Off"
+  }\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
 client.distube
-  .on('playSong', (queue, song) =>
+  .on("playSong", (queue, song) =>
     queue.textChannel.send(
-      `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
-        song.user
-      }\n${status(queue)}`
+      `${client.emotes.play} | Playing \`${song.name}\` - \`${
+        song.formattedDuration
+      }\`\nRequested by: ${song.user}\n${status(queue)}`
     )
   )
-  .on('addSong', (queue, song) =>
+  .on("addSong", (queue, song) =>
     queue.textChannel.send(
       `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
     )
   )
-  .on('addList', (queue, playlist) =>
+  .on("addList", (queue, playlist) =>
     queue.textChannel.send(
       `${client.emotes.success} | Added \`${playlist.name}\` playlist (${
         playlist.songs.length
       } songs) to queue\n${status(queue)}`
     )
   )
-  .on('error', (channel, e) => {
-    channel.send(`${client.emotes.error} | An error encountered: ${e.toString().slice(0, 1974)}`)
-    console.error(e)
+  .on("error", (channel, e) => {
+    channel.send(
+      `${client.emotes.error} | An error encountered: ${e
+        .toString()
+        .slice(0, 1974)}`
+    );
+    console.error(e);
   })
-  .on('empty', channel => channel.send('Voice channel is empty! Leaving the channel...'))
-  .on('searchNoResult', (message, query) =>
-    message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`)
+  .on("empty", (channel) =>
+    channel.send("Voice channel is empty! Leaving the channel...")
   )
-  .on('finish', queue => queue.textChannel.send('Finished!'))
+  .on("searchNoResult", (message, query) =>
+    message.channel.send(
+      `${client.emotes.error} | No result found for \`${query}\`!`
+    )
+  )
+  .on("finish", (queue) => queue.textChannel.send("Finished!"));
